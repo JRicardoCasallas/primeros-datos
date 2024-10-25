d2044760ba8354e165a40e5b89af64f660fd5686
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-10-24 21:18:23 -0500

    primeros datos

---
+++ index.js
@@ -0,0 +1,8 @@
+const nombre = "Ricardo";
+const apellido = "casallas";
+const telefono = 3124122863;
+const direccion = "calle 12"
+const ciudad = "bogota";
+
+
+console.log("nombre, apellido, telefono direccion, ciudad");
